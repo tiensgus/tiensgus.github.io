e65bb12d217f4ca2466e4e65fec05195adcb83d0
tiensgus <tiensgus@hotmail.com>
2025-06-28 04:43:28 -0500

    Add files via upload

--- indexvoa.js
+++ indexvoa.js
@@ -194,4 +194,130 @@ function chonUnitVoa(){
     iframe_ytEl.src="https://youtube.com/embed/" + videoIDcurrentVoa;
 
 }
+
+//-------
+let lIdTitle_driver = [
+"CCCUTqJ3dBA|Learn How to Drive Car",
+"CCCUTqJ3dBA|001: Learn How to Drive",
+"ud8tM2OXcmY|002: Learn How to Drive Car Controls",
+"ippjq7-QMS8|003: How to Drive a Car in a Straight Line",
+"F3lIyv20Y54|004: How To Control The Gas and Brake Pedals",
+"l2Vq6KkLolY|005: LEARN HOW TO MERGE  onto Highway Safely",
+"2cjcJv2G_qg|006: How To Pass Your Driver’s Test",
+"i-khzo9ApcY|007: How To Do a Three Point Turn",
+"icLfRpbFdGE|008: To Control Gas & Brake Pedal in a CAR",
+"W1FISN536HY|009: Techniques to Pass Your Road Test First Time",
+"5ojncFo1HNk|010: Learning to Drive a Car",
+"iF5wZnNPEIw|011: Park in Reverse",
+"9f4ioms-7Uc|012: What to Practice/Where to Start",
+"P2jdHup_vlY|013: How to Drive in a Straight Line",
+"ORuPdMPLz9Y|014: How To Change Lanes",
+"RNeRrcpmTF0|015: How to Pass Your Drivers Exam",
+"kXxEMuhr1sw|016: Learn How to Parallel Park",
+"N4JWp2YKQqo|017: IMPORTANT POINT IN THE DRIVING ROAD TEST",
+"gu1KKcyKSmI|018: First Time Behind the Wheel",
+"Ss6Gtpd5lVs|019: NEW 2023 Driving Test",
+"T9f1Rt3N87g|020: The Secret To Making Perfect Turns While Driving",
+"A5QkPbBKLlI|021: Master the Basics",
+"0Ni9trBmLq4|022: Top Mistakes to Avoid and Pass Your Driving Exam",
+"Ukk8s44BvZc|023: Lane Change Made Easy",
+"mkDQRVruJ9c|024: How To Change Lanes",
+"vmP_nQbmuPs|025: How to Exit Parking Spaces Like a Pro",
+"oq0SB8mXErw|026: How To Stay Centered in Your Lane",
+"bCYWfsVRl0c|027: How To Use Your Mirrors",
+"VKD4LjLV0tw|028: Reverse Parking",
+"5r87CvbrsAo|029: Lane Change Techniques: A Must-Watch Tutorial",
+"5DYuuV9FQzo|030: First Time Behind the Wheel",
+"kvuUSpZU44A|031: The DMV Test Guide",
+"8Vt3z2UH1jI|032: Ace Your Written Driving Test!",
+"PuFFijGgP7E|033: Mistakes To Avoid If You Want To Pass Your Road",
+"RTvywSBGzgo|034: How To Obtain Your Driver's License",
+"qE46UxUOv4c|035: The 10 Must-Knows for Beginners-Essential Tips",
+"o_Fvye2NWGw|036: Unlock Your Confidence: *Top Tips Revealed*",
+"Hkgsc4Wu28Q|037: Crush Your Parallel Parking Like a Boss!",
+"tGfLZGAk_sQ|038: Learn To Reverse Out of a Parking Space",
+"Ld-uWn2Mf3A|039: 3-Point Turns on Your Road Test!",
+"oB0kP5ilvts|040: How To Make Turns and Control The Steering Wheel",
+"TAoc4ZP_Y1M|041: How To Pass Your Driving Exam",
+"1ivMkhtGq4o|042: How To Drive On The Highway: Merging Techniques",
+"B6KVfzWt6EY|043: Learn to Drive Stick",
+"Uv1xCITxIxM|044: Lane-Keeping Tips for New Drivers",
+"LSv10gVeiCA|045: Navigating Your Car's Controls for Beginners",
+"vy6R-PxbLeQ|046: The Ultimate Traffic Sign Tutorial",
+"JgObYyIWMwA|047: What You Need To Know Before You Drive!",
+"PTSonLCKWu4|048: Your Road Test Survival Guide",
+"0e4JjNryU90|049: Overcoming Fear as a Beginner Driver",
+"HWzD4RnUvMk|050: Learn How to Park",
+"YhvWXhZOMfQ|051: Driving for the First Time: Step-by-Step Tutorial",
+"E2uXiQ91m2k|052: Mistakes That Will Fail Your Driving Test",
+"oYALXa3Y9I0|053: How to Accelerate and Brake Smoothly",
+"EpGlJxRlrgc|054: How To Make Turns",
+"QBxFs5Ms6Q4|055: Your First Time on the Highway",
+"yYL9cvZWRUI|056: Overcoming Driving Anxiety Fast",
+"pyTHd_DP15o|057: Pass The Road Test",
+"vn743_v_tyU|058: Driver's License 101",
+"6yOLoyq93BA|059: How to Handle Your First Drive in Traffic",
+"3A3RgJkojQY|060: 3-Point Turn Made Easy",
+"7XGwwH5ZgxA|061: What to Do When Pulled Over",
+"bA-VPf7eQ8A|062: How to Use Your Mirrors",
+"JC-VPFa2ox8|063: DMV Written Test Prep",
+"QS2Miq5m9N8|064: Parking For The First Time",
+"nKAWIhmoocY|065: Parallel Parking For The First Time",
+"MfKz5nKMI9M|066: How to Reverse Park Your Car",
+"-jxdQTSEXEo|067: Road Signs & Signals",
+"aVKM2Z9eePM|068: Your First Driving Lesson",
+"OaQEsDScaBU|069: Driving in Traffic For The First Time",
+"SxtqKhiZvFA|070: How to Pass Your Road Test First Time",
+"E4-_s2Tbz4Q|071: Top 5 Mistakes That Fail Your Driving",
+"3gkcKYViKkc|072: Straight Parking 101",
+"6j83BCu7CGs|073: Easy 3-Point Turn",
+"rGIFKjNtugw|074: How To Make Turns",
+"xFUiVnPT-e8|075: How To Make Turns for Drivers",
+"3P5lLbdOEE0|076: Go From Zero to Confident Driver",
+"GkKjHCtunes|077: Parallel Parking: The EASIEST Way Ever!",
+"y_vH9OyP5zs|078: Lane Changing for Beginners",
+"nHcG4_eTmMg|079: How to Drive On The Highway",
+"CZXU-CRPCKs|080: Must-Know Reversing Tips to Pass Your Driving",
+"f4jcZshsmVU|081: Is Tesla the Best Electric Car",
+"0znTbKmCRz4|082: How to Get ROAD TEST READY in 24 Hours!",
+"Y_HcH6BSK60|083: How to Stay in Your Lane",
+"tWQ5xYNnJtM|084: Use Pedals, Adjust Seat, & Set Mirrors!",
+"EMCzxnhc6PQ|085: PASS your Driving avoiding these mistakes!",
+"Dvpw0XbsPBU|086: Road Sign and Street Signal You Need to Know!",
+"OPjrBtX1e2M|087: ROAD TEST DAY",
+"H_4blrvHvhI|088: How to Exit a Parking Space",
+"boAHA39qXpI|089: How To Park a Car",
+"Pke1Ix4A7Ms|090: How To Turn The Steering Wheel",
+"kF4SbzZiPBQ|091: CDL PRACTICE TEST 2025: Pre-Trip Inspection",
+"bpC3FR-Vidg|092: CDL PRACTICE TEST 2025: Hazmat Endorsement",
+"mm6w3ccNxb4|093: Three-Point Turn Tutorial",
+"MW1tr5j3zPA|094: Gas and Brake Control",
+"EXzcsEv_jZg|095: How to Pass Your Road Test",
+"cRUWI4ErLQU|096: Overcoming Driving Anxiety",
+"70zPW7t5_eQ|097: How to Get Your Driver's License",
+"jt5zc8EYRGc|098: Your Last Minute GUIDE to pass!",
+"l4Z13XGxFlA|099: Learn Traffic Signs & Street Markings",
+"tpYM_xaTsOA|100: How To Use Your Vehicle's Blind Spots"
+]
+const selectdriver = document.getElementById("select_driver");
+
+function updateMenuD() {
+    for (let i = 0; i < lIdTitle_driver.length; i++) {
+        let optn = lIdTitle_driver[i].split("|")[1];
+        let el = document.createElement("option");
+        el.textContent = optn;
+        el.value = optn;
+        console.log(el.value);
+        selectdriver.appendChild(el);
+    }
+}
+
+updateMenuD();
+
+function chonUnitDriver(){
+    videoIDcurrentDriver = lIdTitle_driver[selectdriver.selectedIndex].split("|")[0];
+    iframe_ytEl.src="https://youtube.com/embed/" + videoIDcurrentDriver;
+
+}
+
 //---------------------------
